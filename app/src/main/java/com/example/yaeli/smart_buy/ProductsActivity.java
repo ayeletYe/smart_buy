ecd261c4e308c2f460db46a6c464c06ced5b33fd
sbhtkx <danielsaban2015@gmail.com>
2018-01-09 12:12:54 +0200

    daniel: finished with tables

--- app/src/main/java/com/example/yaeli/smart_buy/ProductsActivity.java
+++ app/src/main/java/com/example/yaeli/smart_buy/ProductsActivity.java
@@ -2,12 +2,66 @@ package com.example.yaeli.smart_buy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ProductsActivity extends AppCompatActivity {
+
+    private DatabaseReference databaseReference;
+    private ListView mListView;
+    private ArrayList<String> mProducts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
+
+        databaseReference= FirebaseDatabase.getInstance().getReference();
+
+        mProducts = new ArrayList<>();
+
+        mListView = (ListView) findViewById(R.id.listViewProducts);
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mProducts);
+        mListView.setAdapter(arrayAdapter);
+
+        final String[] featuresEng ={"name","producer","calories","protein","carbohydrates","sugars","total fat","saturated fat","trans fat" ,"cholesterol","sodium"};
+        final String[] featuresHe  ={"שם",  "יצרן",    "קלוריות", "חלבונים","פחמימות" ,     "סוכרים","שומנים" ,  "שומן רווי",    "שומן טרנס"  ,"קולסטרול",  "נתרן"};
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot d:dataSnapshot.child("products").getChildren()){
+
+                    String value="";
+                    for(int  i= 0; i<featuresEng.length; i++){
+                        System.out.println(value);
+                        System.out.println(featuresEng[i]);
+                        System.out.println(d.hasChild(featuresEng[i]));
+                        if(d.hasChild(featuresEng[i])) {
+                            value += featuresHe[i] + ": " + d.child(featuresEng[i]).getValue()+"\n";
+                        }
+                    }
+                    mProducts.add(value);
+                }
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/yaeli/smart_buy/inStockProductsActivity.java
+++ app/src/main/java/com/example/yaeli/smart_buy/inStockProductsActivity.java
@@ -2,19 +2,63 @@ package com.example.yaeli.smart_buy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class inStockProductsActivity extends AppCompatActivity {
-    DatabaseReference databaseReference;
+
+    private DatabaseReference databaseReference;
+    private ListView mListView;
+    private ArrayList<String> mProductsInStock;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_stock_products);
         databaseReference= FirebaseDatabase.getInstance().getReference();
 
+        mProductsInStock = new ArrayList<>();
 
+        mListView = (ListView) findViewById(R.id.listViewStock);
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mProductsInStock);
+        mListView.setAdapter(arrayAdapter);
+
+        final String[] featuresEng ={"name","quantity"};
+        final String[] featuresHe  ={"שם",  "מוצרים בחנות"};
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot d:dataSnapshot.child("stock").getChildren()){
+
+                    String value="";
+                    for(int  i= 0; i<featuresEng.length; i++){
+                        System.out.println(value);
+                        System.out.println(featuresEng[i]);
+                        System.out.println(d.hasChild(featuresEng[i]));
+                        if(d.hasChild(featuresEng[i])) {
+                            value += featuresHe[i] + ": " + d.child(featuresEng[i]).getValue()+"\n";
+                        }
+                    }
+                    mProductsInStock.add(value);
+                }
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
